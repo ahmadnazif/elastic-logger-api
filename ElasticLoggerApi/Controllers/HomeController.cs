9726a1994ba5beb3f318fe8eddf5f6a3cb446057
Nazif Abdullah <ahmadnazif@gmail.com>
2023-10-11 16:35:31 +0800

    Add HomeController

---
+++ ElasticLoggerApi/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElasticLoggerApi.Controllers;
+
+[ApiExplorerSettings(IgnoreApi = true)]
+[Route("/")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Home() => Redirect("/swagger/index.html");
+}
--- ElasticLoggerApi/Program.cs
+++ ElasticLoggerApi/Program.cs
@@ -1,23 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+var config = builder.Configuration;
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(x => x.AddDefaultPolicy(y => y.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+
+builder.WebHost.ConfigureKestrel(x =>
+{
+    var httpPort = int.Parse(config["Port"]);
+    x.ListenAnyIP(httpPort);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
